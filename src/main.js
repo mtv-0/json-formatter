b068695f556cf2b6227c177859e3994004c314d5
Walter Tavares <walter.tavares@paganini.tech>
2025-10-10 23:45:41 -0300

    highlight

--- src/main.js
+++ src/main.js
@@ -3,13 +3,39 @@ const outputArea = document.querySelector(".large-area--output");
 const formatButton = document.querySelector(".controls__button--format");
 const minifyButton = document.querySelector(".controls__button--minify");
 
+function syntaxHighlight(json) {
+  json = json
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+  return json.replace(
+    /("(\\u[\da-fA-F]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(\.\d*)?([eE][+\-]?\d+)?)/g,
+    function (match) {
+      let cls = "number";
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = "key";
+        } else {
+          cls = "string";
+        }
+      } else if (/true|false/.test(match)) {
+        cls = "boolean";
+      } else if (/null/.test(match)) {
+        cls = "null";
+      }
+      return `<span class="${cls}">${match}</span>`;
+    }
+  );
+}
+
 formatButton.addEventListener("click", () => {
   try {
     const json = JSON.parse(inputArea.value);
     const formattedJson = JSON.stringify(json, null, 2);
-    outputArea.value = formattedJson;
+    outputArea.innerHTML = syntaxHighlight(formattedJson);
   } catch (error) {
-    outputArea.value = "Invalid JSON";
+    outputArea.textContent = `Invalid JSON! \n
+${error.message}`;
   }
 });
 
@@ -17,8 +43,8 @@ minifyButton.addEventListener("click", () => {
   try {
     const json = JSON.parse(inputArea.value);
     const minifiedJson = JSON.stringify(json);
-    outputArea.value = minifiedJson;
+    outputArea.textContent = minifiedJson;
   } catch (error) {
-    outputArea.value = "Invalid JSON";
+    outputArea.textContent = "Invalid JSON";
   }
 });
